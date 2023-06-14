0341385410b6491f1cb03f62473c03b6277dd372
JeffersonRR <jeffersonrrocon@gmail.com>
2023-06-14 15:35:04 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('home 1')
+    return render(request, 'recipes/home.html')
 
 
 def sobre(request):
