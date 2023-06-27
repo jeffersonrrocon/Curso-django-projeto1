6ed066c8f83b1488b73731463fde5d90f9c875ae
jeffersonrrocon <109685553+jeffersonrrocon@users.noreply.github.com>
2023-06-27 14:58:53 -0300

    404

--- recipes/views.py
+++ recipes/views.py
@@ -1,27 +1,34 @@
-from django.shortcuts import render
+
+from django.shortcuts import get_list_or_404, render
+
 from utils.recipes.factory import make_recipe
-from .models import Recipe
+
+from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
-        ).order_by('-id')
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category_id=category_id,
-        is_published=True
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
         ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first(0).category.name} - Category | '
     })
-    
-    
+
+
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
